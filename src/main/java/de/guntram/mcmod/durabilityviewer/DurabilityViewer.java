8082c58fdcdb4ae1297db553131ca08a5380813c
Guntram Blohm <gbl@bso2001.com>
2017-07-26 21:53:58 +0200

    Version 1.3: set window title to user/server

--- src/main/java/de/guntram/mcmod/durabilityviewer/DurabilityViewer.java
+++ src/main/java/de/guntram/mcmod/durabilityviewer/DurabilityViewer.java
@@ -7,8 +7,14 @@ import de.guntram.mcmod.durabilityviewer.event.KeyInputEvent;
 import de.guntram.mcmod.durabilityviewer.event.TooltipEvent;
 import net.minecraftforge.common.MinecraftForge;
 import de.guntram.mcmod.durabilityviewer.handler.ConfigurationHandler;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.opengl.Display;
 
 @Mod(modid = DurabilityViewer.MODID, 
         name = "Durability Viewer", 
@@ -21,7 +27,7 @@ import net.minecraftforge.fml.common.Mod;
 public class DurabilityViewer
 {
     public static final String MODID = "durabilityviewer";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
 
     @Mod.Instance("durabilityviewer")
     public static DurabilityViewer instance;
@@ -37,9 +43,28 @@ public class DurabilityViewer
     public void init(final FMLInitializationEvent event) {
         KeyHandler.init();
         System.out.println("on Init, confHandler is "+confHandler);
+        MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(confHandler);
         MinecraftForge.EVENT_BUS.register(new KeyInputEvent());
         MinecraftForge.EVENT_BUS.register(new TooltipEvent());
         MinecraftForge.EVENT_BUS.register(new GuiItemDurability());
     }
+    
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public void onConnectedToServerEvent(FMLNetworkEvent.ClientConnectedToServerEvent event) {
+        if (event.isLocal())
+            return;
+        Minecraft mc=Minecraft.getMinecraft();
+        Display.setTitle(mc.getSession().getUsername() + " on "+
+                (mc.getCurrentServerData().serverName == null ? "unknown" : mc.getCurrentServerData().serverName));
+    }
+    
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public void onDisconnectFromServerEvent(FMLNetworkEvent.ClientDisconnectionFromServerEvent event) {
+        Minecraft mc=Minecraft.getMinecraft();
+        Display.setTitle(mc.getSession().getUsername() + " not connected");
+    }
+    
 }
