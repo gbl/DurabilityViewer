bb495833cd5afe214e2caecd561066c111659a36
Guntram Blohm <gbl@guntram.de>
2018-12-27 20:11:36 +0100

    Change KeyBinds from malilib to MineCraft Give the refmap a unique name to prevent collisions with other mods

--- src/main/java/de/guntram/mcmod/durabilityviewer/DurabilityViewer.java
+++ src/main/java/de/guntram/mcmod/durabilityviewer/DurabilityViewer.java
@@ -1,24 +1,28 @@
 package de.guntram.mcmod.durabilityviewer;
 
 import com.mojang.brigadier.CommandDispatcher;
-import de.guntram.mcmod.durabilityviewer.event.InputHandler;
 import de.guntram.mcmod.durabilityviewer.client.gui.GuiItemDurability;
-import de.guntram.mcmod.durabilityviewer.event.KeyInputEvent;
 import de.guntram.mcmod.durabilityviewer.handler.ConfigurationHandler;
 import fi.dy.masa.malilib.config.ConfigManager;
 import fi.dy.masa.malilib.event.InitializationHandler;
-import fi.dy.masa.malilib.event.InputEventHandler;
 import fi.dy.masa.malilib.event.RenderEventHandler;
 import fi.dy.masa.malilib.interfaces.IInitializationHandler;
 import fi.dy.masa.malilib.interfaces.IRenderer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.command.CommandSource;
 import org.dimdev.rift.listener.CommandAdder;
+import org.dimdev.rift.listener.client.KeyBindingAdder;
+import org.dimdev.rift.listener.client.KeybindHandler;
 import org.dimdev.riftloader.listener.InitializationListener;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_H;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
 
-public class DurabilityViewer implements InitializationListener, CommandAdder
+public class DurabilityViewer implements InitializationListener, CommandAdder, KeybindHandler, KeyBindingAdder
 {
     public static final String MODID = "durabilityviewer";
     public static final String VERSION = "1.5";
@@ -26,6 +30,7 @@ public class DurabilityViewer implements InitializationListener, CommandAdder
     public static DurabilityViewer instance;
     private static ConfigurationHandler confHandler;
     private static String changedWindowTitle;
+    private KeyBinding showHide;
 
     @Override
     public void onInitialization() {
@@ -57,13 +62,26 @@ public class DurabilityViewer implements InitializationListener, CommandAdder
         ConfigCommand.register(cd);
     }
 
+    @Override
+    public void processKeybinds() {
+        if (showHide.isPressed()) {
+            GuiItemDurability.toggleVisibility();
+        }
+    }
+
+    @Override
+    public Collection<? extends KeyBinding> getKeyBindings() {
+        List<KeyBinding> myBindings=new ArrayList();
+        
+        myBindings.add(showHide = new KeyBinding("key.duraview", GLFW_KEY_H, "key.categories.durabilityviewer"));
+        return myBindings;
+    }
+
     private static class InitHandler implements IInitializationHandler {
         @Override
         public void registerModHandlers() {
             IRenderer renderer = new GuiItemDurability();
             RenderEventHandler.getInstance().registerGameOverlayRenderer(renderer);
-            InputEventHandler.getInstance().registerKeybindProvider(new InputHandler());
-            ConfigurationHandler.getHotkey().setCallback(new KeyInputEvent());
         }
     }
 }
