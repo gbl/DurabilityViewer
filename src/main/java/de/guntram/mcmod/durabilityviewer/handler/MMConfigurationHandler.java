6dcf489072f1665214b8806b3ff36886264dc29f
Guntram Blohm <gbl@guntram.de>
2020-06-15 18:46:43 +0200

    stop using deprecated ModMenu method

--- src/main/java/de/guntram/mcmod/durabilityviewer/handler/MMConfigurationHandler.java
+++ src/main/java/de/guntram/mcmod/durabilityviewer/handler/MMConfigurationHandler.java
@@ -3,6 +3,7 @@ package de.guntram.mcmod.durabilityviewer.handler;
 import de.guntram.mcmod.durabilityviewer.DurabilityViewer;
 import de.guntram.mcmod.fabrictools.ConfigurationProvider;
 import de.guntram.mcmod.fabrictools.GuiModOptions;
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -18,5 +19,10 @@ public class MMConfigurationHandler implements ModMenuApi
     @Override
     public Optional<Supplier<Screen>> getConfigScreen(Screen screen) {
         return Optional.of(new GuiModOptions(screen, DurabilityViewer.MODNAME, ConfigurationProvider.getHandler(DurabilityViewer.MODNAME)));
+    }
+    
+    @Override
+    public ConfigScreenFactory getModConfigScreenFactory() {
+        return screen -> new GuiModOptions(screen, DurabilityViewer.MODNAME, ConfigurationProvider.getHandler(DurabilityViewer.MODNAME));
     }
 }
