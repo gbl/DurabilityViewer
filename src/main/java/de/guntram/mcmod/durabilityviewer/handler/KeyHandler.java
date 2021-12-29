ccec8c7e1c5d522d2efac860a1e803d4d1d9655b
Guntram Blohm <gbl@guntram.de>
2021-12-29 06:50:32 +0100

    1.18.1

--- src/main/java/de/guntram/mcmod/durabilityviewer/handler/KeyHandler.java
+++ src/main/java/de/guntram/mcmod/durabilityviewer/handler/KeyHandler.java
@@ -1,7 +1,7 @@
 package de.guntram.mcmod.durabilityviewer.handler;
 
 import net.minecraft.client.KeyMapping;
-import net.minecraftforge.fmlclient.registry.ClientRegistry;
+import net.minecraftforge.client.ClientRegistry;
 
 public class KeyHandler
 {
