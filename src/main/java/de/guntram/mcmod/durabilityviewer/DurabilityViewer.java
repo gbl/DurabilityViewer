7747a8109d21dac4269a6f4a49a3f1bf3c1a3300
Guntram Blohm <gbl@guntram.de>
2022-03-13 10:16:45 +0100

    1.18.2

--- src/main/java/de/guntram/mcmod/durabilityviewer/DurabilityViewer.java
+++ src/main/java/de/guntram/mcmod/durabilityviewer/DurabilityViewer.java
@@ -5,11 +5,10 @@ import de.guntram.mcmod.durabilityviewer.client.gui.GuiItemDurability;
 import de.guntram.mcmod.durabilityviewer.handler.ConfigurationHandler;
 import de.guntram.mcmod.fabrictools.ConfigurationProvider;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
-import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
-import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.util.Identifier;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_H;
 
 public class DurabilityViewer implements ClientModInitializer
@@ -20,7 +19,7 @@ public class DurabilityViewer implements ClientModInitializer
     public static DurabilityViewer instance;
     private static ConfigurationHandler confHandler;
     private static String changedWindowTitle;
-    private FabricKeyBinding showHide;
+    private KeyBinding showHide;
     
     @Override
     public void onInitializeClient() {
@@ -48,11 +47,7 @@ public class DurabilityViewer implements ClientModInitializer
 
     public void setKeyBindings() {
         final String category="key.categories.durabilityviewer";
-        KeyBindingRegistry.INSTANCE.addCategory(category);
-        KeyBindingRegistry.INSTANCE.register(
-            showHide = FabricKeyBinding.Builder
-            .create(new Identifier("durabilityviewer:showhide"), InputUtil.Type.KEYSYM, GLFW_KEY_H, category)
-            .build());
-        ClientTickCallback.EVENT.register(e->processKeyBinds());
+        KeyBindingHelper.registerKeyBinding(showHide = new KeyBinding("key.durabilityviewer.showhide", InputUtil.Type.KEYSYM, GLFW_KEY_H, category));
+        ClientTickEvents.END_CLIENT_TICK.register(e->processKeyBinds());
     }    
 }
